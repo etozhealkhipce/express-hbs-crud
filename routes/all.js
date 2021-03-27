6247bd8fb828a7c69bcf349931496ca49976f2f7
Dyomin Ilya <alkhipce@mail.ru>
2021-03-27 18:12:28 +0600

    add one book page

--- routes/all.js
+++ routes/all.js
@@ -1,13 +1,26 @@
 const { Router } = require('express')
+const Book = require('../models/book')
 
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const books = await Book.getAll()
+
   res.status(200)
   // res.sendFile(path.join(__dirname, 'views', 'index'))
   res.render('all', {
     title: 'Main page',
-    isAll: true
+    isAll: true,
+    books
+  })
+})
+
+router.get('/:id', async (req, res) => {
+  const book = await Book.getById(req.params.id)
+  res.render('book', {
+    layout: 'empty',
+    title: `Курс ${book.title}`,
+    book
   })
 })
 
