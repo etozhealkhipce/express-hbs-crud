797a46d2c5c5adb7044f36526813a0077c58bf2d
Dyomin Ilya <alkhipce@mail.ru>
2021-05-02 23:21:02 +0600

    feat: done with db in json

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
+const toCurrency = (price) =>
+  new Intl.NumberFormat('ru-RU', {
+    currency: 'usd',
+    style: 'currency'
+  }).format(price)
+
 // eslint-disable-next-line no-undef
 document.querySelectorAll('.price').forEach((node) => {
   // eslint-disable-next-line no-param-reassign
-  node.textContent = new Intl.NumberFormat('ru-RU', {
-    currency: 'usd',
-    style: 'currency'
-  }).format(node.textContent)
+  node.textContent = toCurrency(node.textContent)
 })
 
 // eslint-disable-next-line no-undef
@@ -19,7 +22,29 @@ if ($cart) {
         method: 'delete'
       })
         .then((res) => res.json())
-        .then((cart) => console.log(cart))
+        .then((cart) => {
+          if (cart.books.length) {
+            const html = cart.books
+              .map(
+                (b) => `<tr>
+              <td class="is-centered">${b.title}</td>
+              <td>${b.price}</td>
+              <td>${b.count}</td>
+              <td>
+                <button type="submit" class="button is-text is-danger remove" data-id="${b.id}">
+                  Remove
+                </button>
+              </td>
+            </tr>`
+              )
+              .join('')
+
+            $cart.querySelector('tbody').innerHTML = html
+            $cart.querySelector('.price').textContent = toCurrency(cart.price)
+          } else {
+            $cart.innerHTML = '<h1 class="title text-is-centered">Cart empty</h1>'
+          }
+        })
     }
   })
 }
